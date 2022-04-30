98ae41a9f7da35752f8bf9161929aa9aa901fc34
Henrique Marques <93006999+Euq1rneh@users.noreply.github.com>
2022-04-30 10:46:22 +0100

    Add files via upload

---
+++ TextComparator/src/RunComparator.java
@@ -0,0 +1,16 @@
+
+public class RunComparator {
+
+	public static void main(String[] args) {
+		
+		//Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	TextComparatorGUI gui = new TextComparatorGUI();                
+            }
+        });
+		
+	}
+
+}
